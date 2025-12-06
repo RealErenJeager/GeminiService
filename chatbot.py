815b25a762356c97f5198d665782f18b9dcd36c5
RealErenJeager <131956473+RealErenJeager@users.noreply.github.com>
2025-12-06 23:03:50 +0500

    Update API URL to use gemini-1.5-flash-8k model

--- chatbot.py
+++ chatbot.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
-URL = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key={API_KEY}"
+URL = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-8k:generateContent?key={API_KEY}"
 
 app = FastAPI()
 
